2dde473713e1b70688faf8bb95e3d1c1a41c519e
Josh Novak <jnovak@ceiamerica.com>
2021-03-15 17:04:18 -0400

    weights

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,11 +14,13 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
             Students = new List<Student>();
+            this.IsWeighted = isWeighted;
         }
 
         public void AddStudent(Student student)
@@ -107,20 +109,25 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            var weight = 0;
+            if( IsWeighted && (studentType == StudentType.DualEnrolled || studentType == StudentType.Honors))
+            {
+                weight = 1;
+            }
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + weight;
                 case 'B':
-                    return 3;
+                    return 3 + weight;
                 case 'C':
-                    return 2;
+                    return 2 + weight;
                 case 'D':
-                    return 1;
+                    return 1 + weight;
                 case 'F':
-                    return 0;
+                    return 0 + weight;
             }
-            return 0;
+            return 0 + weight;
         }
 
         public virtual void CalculateStatistics()
